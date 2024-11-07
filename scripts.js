d002e1a7ca5d5e05ff4f40aaad0a1a5aa6a5c86c
MaverickCod <921chavanprem@gmail.com>
2024-11-07 11:23:46 +0530

    Completed Emoji Picker Integration

--- scripts.js
+++ scripts.js
@@ -133,6 +133,27 @@ fileCancelButton.addEventListener("click", () => {
     userData.file = {};
     fileUploadWrapper.classList.remove("file-uploaded")
 });
+
+// Initilize emoji picker & handle emoji selection
+const picker = new EmojiMart.Picker({
+    theme: "light",
+    skinTonePosition :"none",
+    previewPosition :"none",
+    onEmojiSelect : (emoji) => {
+        const{selectionStart : start, selectionEnd: end} = messageInput;
+        messageInput.setRangeText(emoji.native, start, end, "end");
+        messageInput.focus();
+    },
+    onClickOutside : (e) => {
+        if(e.target.id === "emoji-picker") {
+            document.body.classList.toggle("show-emoji-picker");
+        } else {
+            document.body.classList.remove("show-emoji-picker");
+        }
+    }
+});
+
+document.querySelector(".chat-form").appendChild(picker);
  
 sendMessageButton.addEventListener("click", (e) => handleOutgoingMessage(e));
 document.querySelector("#file-upload").addEventListener("click", () => fileInput.click());
